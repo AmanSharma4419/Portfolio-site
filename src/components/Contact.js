6b972e9ae88c7d9527b05e9497bbfa3a236a594c
AmanSharma4419 <Sharmaaman4419@gmail.com>
2020-05-17 17:38:55 +0100

    --implemented the contact form with email generation--

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
+import Navbar from "./Navbar";
 
 export default function Contact() {
   const [name, setname] = useState("");
@@ -7,18 +8,26 @@ export default function Contact() {
   const [message, setmessage] = useState("");
 
   let templateParams = {
+    to_name: "Aman Sharma",
     from_name: name,
     from_email: email,
+    reply_to: email,
     message_html: message,
   };
 
   const handleSubmit = (e) => {
+    console.log(
+      process.env.REACT_APP_API_gmail,
+      process.env.REACT_APP_API_template,
+      templateParams,
+      process.env.REACT_APP_API_user
+    );
     emailjs
       .send(
-        process.env.Service,
-        process.env.TEMPLATE_ID,
-        templateParams,
-        process.env.userID
+        process.env.REACT_APP_API_gmail,
+        process.env.REACT_APP_API_template,
+        process.env.templateParams,
+        process.env.REACT_APP_API_user
       )
       .then(
         function (response) {
@@ -31,60 +40,80 @@ export default function Contact() {
   };
   return (
     <>
-      <div class="field">
-        <label class="label">Name</label>
-        <div class="control">
-          <input
-            class="input"
-            type="text"
-            placeholder="Text input"
-            onChange={(e) => {
-              setname(e.target.value);
-            }}
-          />
+      <Navbar />
+      <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+          <div class="container">
+            <h1 class="title">Start a conversation.</h1>
+            <hr />
+            <h2 class="subtitle">
+              If something sparked you, touched you, or you have a feedback,
+              don't deprive me of a heartwarming message.
+            </h2>
+          </div>
         </div>
-      </div>
+      </section>
+      <section class="hero is-light">
+        <div style={{ width: "40%", marginLeft: "10px", marginTop: "20px" }}>
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder="Enter Name"
+                onChange={(e) => {
+                  setname(e.target.value);
+                }}
+              />
+            </div>
+          </div>
 
-      <div class="field">
-        <label class="label">Email</label>
-        <div class="control has-icons-left has-icons-right">
-          <input
-            class="input is-danger"
-            type="text"
-            placeholder="Email input"
-            onChange={(e) => {
-              setemail(e.target.value);
-            }}
-          />
-        </div>
-      </div>
+          <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input
+                type="Email"
+                class="input"
+                placeholder="Email input"
+                onChange={(e) => {
+                  setemail(e.target.value);
+                }}
+              />
+            </div>
+          </div>
 
-      <div class="field">
-        <label class="label">Message</label>
-        <div class="control">
-          <textarea
-            class="textarea"
-            type="text"
-            placeholder="Textarea"
-            onChange={(e) => {
-              setmessage(e.target.value);
-            }}
-          ></textarea>
-        </div>
-      </div>
+          <div class="field">
+            <label class="label">Message</label>
+            <div class="control">
+              <textarea
+                class="textarea"
+                type="text"
+                placeholder="Enter Text"
+                onChange={(e) => {
+                  setmessage(e.target.value);
+                }}
+              ></textarea>
+            </div>
+          </div>
 
-      <div class="field is-grouped">
-        <div class="control">
-          <button
-            class="button is-link"
-            onClick={() => {
-              handleSubmit();
-            }}
+          <div
+            class="field is-grouped"
+            style={{ display: "flex", justifyContent: "center" }}
           >
-            Submit
-          </button>
+            <div class="control">
+              <button
+                class="button is-link"
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
+                Submit
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,67 +4,75 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <Link to="/" class="navbar-item">
-              Home
-            </Link>
+      <section class="hero is-primary">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <Link to="/" class="navbar-item">
+                Home
+              </Link>
 
-            <Link to="/about" class="navbar-item">
-              About
-            </Link>
-            <Link to="/skills" class="navbar-item">
-              Skills
-            </Link>
-            <Link to="/contact" class="navbar-item">
-              Contact
-            </Link>
+              <Link to="/about" class="navbar-item">
+                About
+              </Link>
+              <Link to="/skills" class="navbar-item">
+                Skills
+              </Link>
+              <Link to="/contact" class="navbar-item">
+                Contact
+              </Link>
 
-            <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">More</a>
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" >More</a>
 
-              <div class="navbar-dropdown">
-                <a
-                  href="https://amansharmablog.netlify.app/"
-                  class="navbar-item"
-                >
-                  Blog
-                </a>
-                <a href="https://github.com/AmanSharma4419" class="navbar-item">
-                  Github
-                </a>
-                <a href="https://twitter.com/amansharma214" class="navbar-item">
-                  Twitter
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/aman-sharma-b21b68196/"
-                  class="navbar-item"
-                >
-                  Linkdien
-                </a>
+                <div class="navbar-dropdown">
+                  <a
+                    href="https://amansharmablog.netlify.app/"
+                    class="navbar-item" style={{color:"black"}}
+                  >
+                    Blog
+                  </a>
+                  <a
+                    href="https://github.com/AmanSharma4419"
+                    class="navbar-item" style={{color:"black"}}
+                  >
+                    Github
+                  </a>
+                  <a
+                    href="https://twitter.com/amansharma214"
+                    class="navbar-item" style={{color:"black"}}
+                  >
+                    Twitter
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/aman-sharma-b21b68196/"
+                    class="navbar-item"style={{color:"black"}}
+                  >
+                    Linkdien
+                  </a>
 
-                <hr class="navbar-divider" />
-                <a class="navbar-item">Report an issue</a>
+                  <hr class="navbar-divider" />
+                  <a class="navbar-item" style={{color:"black"}}>Report an issue</a>
+                </div>
+              </div>
+            </div>
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <Link
+                    to="/"
+                    style={{ boxSizing: "border-box", padding: "3.5px" }}
+                  >
+                    <h2 style={{ color: "white", fontFamily: "sans" }}>
+                      AMAN SHARMA
+                    </h2>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <Link
-                  to="/"
-                  style={{ boxSizing: "border-box", padding: "3.5px" }}
-                >
-                  <h2 style={{ color: "black", fontFamily: "sans" }}>
-                    AMAN SHARMA
-                  </h2>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </section>
     </>
   );
 }
